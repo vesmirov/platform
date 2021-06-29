461778896b8838e7ad5d6a1dce0447a923e431e5
Evan Vilagov <evan.vilagov@gmail.com>
2021-06-29 16:47:51 +0300

    Edited: about.html (now it's adaptive). Added: some information about author and used tech, plus some pictures

--- flatpages/views.py
+++ flatpages/views.py
@@ -4,29 +4,30 @@ from django.views.generic.base import TemplateView
 class AuthorPage(TemplateView):
     template_name = 'flatpages/about.html'
     extra_context = {
-        'page': 'author',
+        'author': True,
         'title': 'Evan Vilagov',
         'content_title': 'About author',
-        'github': 'vilagov',
-        'instagram': 'evan.vilagov/',
-        'linkedin': 'vilagov',
-        'facebook': 'vilagov',
+        'links': {
+            'Github': 'https://www.github.com/vilagov/',
+            'Linkedin': 'https://www.linkedin.com/in/vilagov/',
+            'Instagram': 'https://www.instagram.com/evan.vilagov/',
+            'Facebook': 'https://www.facebook.com/vilagov/vilagov',
+        },
         'email': 'evan.vilagov@gmail.com',
-        'text_ru': '',
-        'text_en': '',
+        'text_ru': 'Информация об авторе на русском.',
+        'text_en': 'Information about author in English',
     }
 
 
 class TechPage(TemplateView):
     template_name = 'flatpages/about.html'
     extra_context = {
-        'page': 'tech',
-        'title': 'Used technologies',
+        'title': 'Stack used',
         'content_title': 'Список использованных технологий',
-        'content': [
-            'Python 3',
-            'Django 3.1',
-            'Django REST Framework 3',
-            'PostgreSQL',
-        ],
+        'links': {
+            'Python 3': '#',
+            'Django 3.1': '#',
+            'Django REST Framework 3': '#',
+            'PostgreSQL': '#',
+        },
     }
